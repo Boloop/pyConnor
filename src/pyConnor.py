598daaa9d11ce10f6443140ca9cda25cf3c387aa
Boloop <boloop@gmail.com>
2013-10-29 19:04:15 +0000

    follows the mouse

--- src/pyConnor.py
+++ src/pyConnor.py
@@ -6,7 +6,11 @@ class App:
         self._running = True
         self._display_surf = None
         self.size = self.weight, self.height = 640, 400
- 
+        
+        
+        self.ball = pygame.Surface((10,10))
+        self.ball.fill((0,255,0))
+        self.ballpos = (0,0)
     def on_init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
@@ -16,9 +20,19 @@ class App:
         if event.type == pygame.QUIT:
             self._running = False
     def on_loop(self):
-        pass
+        """
+        This will run all the logic of the players/menus in the game
+        """
+        self.ballpos = pygame.mouse.get_pos()
     def on_render(self):
-        pass
+        """
+        Render a new scene. and swap the buffer.
+        """
+        self._display_surf.fill((255,0,0))
+        self._display_surf.blit( self.ball, self.ballpos)
+        pygame.display.flip()
+        #pygame.time.Clock.tick(50)
+        
     def on_cleanup(self):
         pygame.quit()
  
